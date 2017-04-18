45ca0ed4277df1fb293da62488a132b529cd64d1
jackey12 <jackey12@centrum.cz>
2017-04-18 11:28:19 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,8 @@ import multiprocessing as mp
 print("Hello world!")
 print("Python version %s " %str(sys.version))
 print("Cores    %s " % str(mp.cpu_count()) )
+
+if 1:
+  print("tady")
+else:
+  print("tady nikdy")
