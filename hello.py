9cf20f7867e19c50b6541bf1b650989f7063d789
jackey12 <jackey12@centrum.cz>
2017-04-18 11:24:50 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,4 +3,4 @@ import multiprocesing as mp
 
 print("Hello world!")
 print("Python version %s " %str(sys.version))
-print("Cores    %s " % mp.cpu_count())
+print("Cores    %s " % str(mp.cpu_count()) )
